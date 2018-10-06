b35755eb2569903e3ded7ee716cad7537cde8906
Packy <packy1014@yahoo.co.jp>
2018-10-07 01:43:09 +0900

    initial

---
+++ directory/src/main/java/com/packy/springboot2/directory/DirectoryApplication.java
@@ -0,0 +1,12 @@
+package com.packy.springboot2.directory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DirectoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DirectoryApplication.class, args);
+	}
+}
